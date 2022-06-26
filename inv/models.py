9a934c5e7baf19f1422c36f11d73db2cae751898
Nikola <mtsebrov@gmail.com>
2022-06-26 23:55:34 +0300

    add web\html\js\css template, reworked inv\models

--- inv/models.py
+++ inv/models.py
@@ -5,16 +5,24 @@ class Parameter(models.Model):
     name = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
     html_type = models.CharField(max_length=20)
+    order = models.IntegerField(default=1)
+    show = models.BooleanField(default=True)
     date = models.DateTimeField(blank=True, auto_now_add=True)
 
+    class Meta:
+        ordering = ['order', ]
+
     def __str__(self):
-        return self.name
+        return f'{self.order}. {self.name}'
 
 
 class InventoryType(models.Model):
     full_name = models.CharField(max_length=20)
     short_name = models.CharField(max_length=5)
-    parameters = models.ManyToManyField(Parameter)
+    parameters = models.ManyToManyField(Parameter, related_name='parameters')
+
+    def __unicode__(self):
+        return self.full_name
 
     def __str__(self):
         return self.full_name
@@ -23,8 +31,12 @@ class InventoryType(models.Model):
 class Inventory(models.Model):
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE, blank=True, null=True)
     type = models.ForeignKey(InventoryType, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateTimeField(blank=True, auto_now_add=True)
     value = models.CharField(max_length=100, blank=True)
     index = models.IntegerField(null=True)
 
     class Meta:
         unique_together = (("type", "parameter", "index"),)
+
+    def __str__(self):
+        return f'{self.index}. {self.type.full_name}-{self.parameter.name}'
